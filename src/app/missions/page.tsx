a8085d73847b2f706e9059229a13c3e36e8df8a7
azkriven <azkriven18@gmail.com>
2025-03-16 14:00:36 +0800

    ✨ feat: update to next15 with turbopack

--- src/app/missions/page.tsx
+++
@@ -1,9 +0,0 @@
-import MissionsDocumentationPage from "@/components/missions";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-    title: "Missions",
-};
-export default function page() {
-    return <MissionsDocumentationPage />;
-}
